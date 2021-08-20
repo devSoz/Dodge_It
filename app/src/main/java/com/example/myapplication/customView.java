ce5a0de130a5b0d088a81342b2bfa3af489a5bc8
Devipriya Sozharajan <devipriyasabi@gmail.com>
2021-08-20 22:30:38 +0530

    Ball move without collision

--- app/src/main/java/com/example/myapplication/customView.java
+++ app/src/main/java/com/example/myapplication/customView.java
@@ -24,14 +24,15 @@ import static android.content.Context.MODE_PRIVATE;
 public class customView extends View
 {
     private Canvas mCanvas;
-    public Float xRunStart, yRunStart, ballSlope, xCanvas, yCanvas, xWall, yWall;
-    public Float xDeltaWall = Float.valueOf(10), yDeltaWall = Float.valueOf(10);
-    public Boolean updateView=true;
+    private Float xRunStart, yRunStart, ballSlope, xCanvas, yCanvas, xWall, yWall,yBottom;
+    private Float xDeltaWall = Float.valueOf(20), yDeltaWall = Float.valueOf(10);
+    private Boolean updateView=true;
     private SoundPool soundPool;
     private RectF endRect;
-    public Integer interval = 30, score;
-    public Ball ballObj;
-    public Wall wallObj;
+    private Integer interval = 40, score, flagClick = 0, flagUp=1, velocity=15;
+    private Ball ballObj;
+    private Integer cnt=0;
+    private Wall wallObj, wallObj2;
 
     private class UpdateViewRunnable implements Runnable {
         public void run()
@@ -39,6 +40,8 @@ public class customView extends View
             //movePong();
             if(updateView) {
                 moveWall();
+                cnt++;
+                if(cnt>=20) cnt=0;
                 postDelayed(this, interval);
             }
         }
@@ -147,15 +150,28 @@ public class customView extends View
             setDefault();
             ballObj = new Ball(xCanvas, yCanvas);
             wallObj = new Wall(xCanvas, yCanvas);
+         //   wallObj2 = new Wall(xCanvas, yCanvas);
         }
 
         Paint paint = new Paint();
         paint.setColor(Color.WHITE);
         paint.setAntiAlias(true);
         mCanvas.drawRect(wallObj.rWall, paint);
-        //mCanvas.drawLine(0,);
 
-        //mCanvas.drawRect(10, 10, 100, 100, paint);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        if(flagClick==1)
+        {
+            moveBall();
+            mCanvas.drawCircle(ballObj.x, ballObj.y, ballObj.radius, paint);
+        }
+        else {
+            mCanvas.drawCircle(ballObj.x, ballObj.y, ballObj.radius, paint);
+        }
+        paint.setColor(getResources().getColor(R.color.foregnd));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20f);
+        mCanvas.drawLine(0,yBottom+15,xCanvas,yBottom+15,paint);
     }
 
     @Override
@@ -164,21 +180,11 @@ public class customView extends View
         xWall= event.getX();
         yWall = event.getY();
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-
-
-                break;
-            }
-
-            case MotionEvent.ACTION_MOVE:
-            {
-
-                break;
-            }
 
             case MotionEvent.ACTION_UP:
             {
-                 break;
+                flagClick = 1;
+                break;
             }
         }
 
@@ -187,12 +193,34 @@ public class customView extends View
         return true;
     }
 
+    public void moveBall()
+    {
+        if(wallObj.rWall.contains((ballObj.x+ballObj.radius), (ballObj.y)+ballObj.radius))
+
+        if(ballObj.y<=(yCanvas/8))
+        {
+            flagUp=0;
+        }
+        if(flagUp==0) {
+            if((ballObj.y+ballObj.radius)>=yBottom) {
+                flagClick = 0;
+                flagUp = 1;
+            }
+            else
+                ballObj.y += velocity;
+        }
+        else
+            ballObj.y-=velocity;
+
+
+    }
+
     private void moveWall()
     {
         if(wallObj.rWall.right<0)
         {
-            wallObj.rWall.top = yCanvas-500;
-            wallObj.rWall.bottom = wallObj.rWall.top + wallObj.height;
+            //wallObj.rWall.top = yCanvas-500;
+          //  wallObj.rWall.bottom = wallObj.rWall.top + wallObj.height;
             wallObj.rWall.left = xCanvas;
             wallObj.rWall.right = wallObj.rWall.left + wallObj.width;
            /* wallObj.rWall.top = yCanvas;
@@ -216,16 +244,14 @@ public class customView extends View
 
         public Ball(Float xCanvas, Float yCanvas)
         {
-            this.radius = 20;
+            this.radius = 30;
+            this.x = xCanvas/10;
+            this.y = yBottom-this.radius;
             Paint paint = new Paint();
             paint.setColor(getResources().getColor(R.color.foregnd));
-            mCanvas.drawCircle(3*(xCanvas/8), xCanvas/8, this.radius, paint);
+            mCanvas.drawCircle(this.x, this.y, this.radius, paint);
         }
 
-        public void moveBall()
-        {
-
-        }
         public void ballHit() {
 
         }
@@ -244,7 +270,7 @@ public class customView extends View
             this.width = 50;
 
             rWall = new RectF();
-            rWall.top = yCanvas-500;
+            rWall.top = yBottom-this.height;
             rWall.bottom = rWall.top + this.height;
             rWall.left = xCanvas;
             rWall.right = rWall.left + this.width;
@@ -259,6 +285,7 @@ public class customView extends View
     {
         xCanvas = Float.valueOf(mCanvas.getWidth());
         yCanvas = Float.valueOf(mCanvas.getHeight());
+        yBottom = yCanvas-(yCanvas/8);
         mCanvas.drawColor(getResources().getColor(R.color.backgnd));
 
         //xRunStart = xCanvas/2;
@@ -267,12 +294,29 @@ public class customView extends View
         //xRandomStart = random.nextInt((int) (xCanvas*0.9+1)-10);
     }
 
+    Boolean intersects()
+    {
+        Float cX = Math.abs(ballObj.x - wallObj.rWall.left);
+        Float cY = Math.abs(ballObj.y - wallObj.rWall.top);
+
+        if (cX > (wallObj.width/2 + ballObj.radius)) { return false; }
+        if (cY > (wallObj.height/2 + ballObj.radius)) { return false; }
+
+        if (cX <= (rect.width/2)) { return true; }
+        if (cY <= (rect.height/2)) { return true; }
+
+        cornerDistance_sq = (circleDistance.x - rect.width/2)^2 +
+                (circleDistance.y - rect.height/2)^2;
+
+        return (cornerDistance_sq <= (circle.r^2));
+    }
+
     public void debugCanvas()
     {
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setTextSize(50);
-        //mcanvas.drawText(String.valueOf(yRandom), 200, 200, paint);
+      //  mCanvas.drawText(String.valueOf(yRandom), 200, 200, paint);
         //mcanvas.drawText(String.valueOf(yCanvas), 100, 300, paint);
         //Log.d("debug", String.valueOf(xRunStart) + "," +String.valueOf(yRunStart) + " delta" +String.valueOf(xDelta) + "," + String.valueOf(yDelta)  );
 
